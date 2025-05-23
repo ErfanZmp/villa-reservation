9cd89ffed8838251d113bbfd98a8622dd82a6bee
Erfan Zamirpour <erfanzamirpour@gmail.com>
2025-05-23 14:43:42 +0000

    middlewhere fixed

--- user-service/app/main.py
+++ user-service/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .routers import auth, users
 from dotenv import load_dotenv
 
 load_dotenv()
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=models.engine)
 
